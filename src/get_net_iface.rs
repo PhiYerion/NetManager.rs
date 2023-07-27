fcbbb067dd2033eb7e284ef648627d591bd93984
phiyerion <reidbandy@gmail.com>
2023-07-27 02:46:28 -0400

    Moved iface filtering logic to proper module

--- src/get_net_iface.rs
+++ src/get_net_iface.rs
@@ -1,16 +1,21 @@
+use std::collections::HashSet;
 use std::ffi::CStr;
 use libc::{self, c_char};
 use std::str;
 
 pub fn get_network_interfaces() -> Vec<String> {
     let mut interfaces = Vec::new();
+    let mut seen_items: HashSet<String> = HashSet::new();
 
     let mut ifc: *mut libc::ifaddrs = std::ptr::null_mut();
     if unsafe { libc::getifaddrs(&mut ifc) } == 0 {
         let mut current = ifc;
         while !current.is_null() {
             if let Some(interface_name) = get_interface_name(current) {
-                interfaces.push(interface_name);
+                if interface_name != "lo" && !seen_items.contains(&*interface_name) {
+                    seen_items.insert(interface_name.clone());
+                    interfaces.push(interface_name.clone());
+                }
             }
             current = unsafe { (*current).ifa_next };
         }
@@ -19,6 +24,7 @@ pub fn get_network_interfaces() -> Vec<String> {
 
     interfaces
 }
+
 
 fn get_interface_name(ifa: *const libc::ifaddrs) -> Option<String> {
     if ifa.is_null() {
