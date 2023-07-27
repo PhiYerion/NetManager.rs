896ce80a5d55aab50e3e124cdba5842e9a3a904a
phiyerion <reidbandy@gmail.com>
2023-07-27 02:45:46 -0400

    Improved CLI

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,62 @@ mod send_dhcp;
 mod get_net_iface;
 mod set_net;
 
+use std::collections::{HashSet};
+use std::io;
+use clap::Parser;
+
 use send_dhcp::get_netmask;
+use crate::get_net_iface::get_network_interfaces;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct inlineArgs {
+    /// Network interface
+    interface: Option<String>
+}
+
+struct Args {
+    interface: String
+}
+
+fn interactive_cli() -> Args {
+    let mut input = String::new();
+    let interfaces = get_network_interfaces();
+
+    while !interfaces.contains(&input) {
+        println!("Choose an interface ({:?})",
+                 get_network_interfaces());
+
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        input = input.trim().to_string();
+    }
+
+    Args{ interface: input }
+}
 
 fn main() {
-    println!("{:?}", get_netmask("enp7s0").unwrap());
+    let inlineArgs = inlineArgs::parse();
+
+    let args = match inlineArgs.interface {
+        Some(interface) => {
+            let interfaces = get_network_interfaces();
+
+            if !interfaces.contains(&interface) {
+                println!("{} not found. Use one of the following interfaces: {:?}",
+                         interface,
+                         interfaces);
+                return;
+            }
+
+            Args { interface }
+        }
+        None => {
+            interactive_cli()
+        }
+    };
+
+    println!("{:?}", get_netmask(&args.interface).unwrap());
 }
