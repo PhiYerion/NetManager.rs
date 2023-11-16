c4ebdbae32d20a788d84b0cea4116556a86099d1
phiyerion <reidbandy@gmail.com>
2023-11-16 13:39:26 -0500

    Formatting

--- src/mac.rs
+++ src/mac.rs
@@ -1,17 +1,12 @@
+use libc::{c_char, c_int, sockaddr, ARPHRD_ETHER};
 use netdevice::{get_hardware, set_hardware};
-use libc::{ARPHRD_ETHER, c_int, c_char, sockaddr};
 use pnet::util::MacAddr;
 use std::io::Error;
 
 pub fn new_socket() -> Result<c_int, Error> {
-    use libc::{AF_INET,
-               IPPROTO_UDP,
-               SOCK_DGRAM};
+    use libc::{AF_INET, IPPROTO_UDP, SOCK_DGRAM};
 
-    let res = unsafe { libc::socket(
-        AF_INET,
-        SOCK_DGRAM,
-        IPPROTO_UDP) };
+    let res = unsafe { libc::socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP) };
 
     match res {
         -1 => Err(Error::last_os_error()),
@@ -19,11 +14,12 @@ pub fn new_socket() -> Result<c_int, Error> {
     }
 }
 
-
-pub fn set_mac (interface: &String, mac: MacAddr) {
-
-    let mut old = get_hardware(
-        new_socket().unwrap(), interface).unwrap().sa_data;
+// This should really have some unit tests, but without a docker
+// container it would be weird.
+pub fn set_mac(interface: &String, mac: MacAddr) {
+    let mut old = get_hardware(new_socket().unwrap(), interface)
+        .unwrap()
+        .sa_data;
 
     let mut new_addr: [u8; 6] = [mac.0, mac.1, mac.2, mac.3, mac.4, mac.5];
     new_addr[0] &= 0xfc;
@@ -32,12 +28,15 @@ pub fn set_mac (interface: &String, mac: MacAddr) {
         old[i] = new_addr[i] as c_char;
     }
 
-    let sock = sockaddr { sa_family: ARPHRD_ETHER, sa_data: old };
+    let sock = sockaddr {
+        sa_family: ARPHRD_ETHER,
+        sa_data: old,
+    };
 
     set_hardware(new_socket().unwrap(), interface, sock).unwrap();
 }
 
-pub fn get_mac (interface: &String) -> [u8; 14] {
+pub fn get_mac(interface: &String) -> [u8; 14] {
     let res = get_hardware(new_socket().unwrap(), interface).unwrap();
 
     // i8 -> u8 is very safe. I hope to god the compiler
