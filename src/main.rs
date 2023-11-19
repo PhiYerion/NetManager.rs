6aa8a1017ea1e718956669de8b29bc55c6015c9b
phiyerion <reidbandy@gmail.com>
2023-11-19 01:00:02 -0500

    Remove allocator. Prefer binary size over small gains

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,12 @@ mod send_dhcp;
 mod subnet_manager;
 mod user_interface;
 
-use clap::Parser;
 use pnet::datalink::interfaces;
 
-use crate::send_dhcp::get_network;
-use crate::user_interface::{cli_get_device_addr, interactive_cli, Args, InlineArgs};
-use local_net::{flush_addresses, get_addresses, get_interface_names, NetworkConfigHandler};
-
-#[global_allocator]
-static GLOBAL: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
+use local_net::NetworkConfigHandler;
 
 #[tokio::main]
 async fn main() {
-    let inline_args = InlineArgs::parse();
     let nch = NetworkConfigHandler::new().unwrap();
     let mut iface_idx = 0;
     for interface in interfaces() {
@@ -29,30 +22,4 @@ async fn main() {
     for address in nch.get_addresses(iface_idx).await {
         println!("Address: {:?}", address);
     }
-
-    /* let args = match inline_args.interface {
-        Some(interface) => {
-            let interfaces = get_interface_names();
-
-            if !interfaces.contains(&interface) {
-                println!(
-                    "{} not found. Use one of the following interfaces: {:?}",
-                    interface, interfaces
-                );
-                return;
-            }
-
-            Args { interface }
-        }
-        None => interactive_cli(),
-    };
-
-    let client_addr = cli_get_device_addr(&args.interface);
-
-
-    let network = get_network(&args.interface).unwrap();
-    dbg!(&network); */
-
-    //down(&args.interface);
-    //up(&args.interface, client_addr).await.unwrap();
 }
