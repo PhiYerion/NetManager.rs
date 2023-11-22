9f4dec2c5f06792799107aee0c04dcb9749eb333
phiyerion <reidbandy@gmail.com>
2023-11-21 22:09:58 -0500

    Better addresses module

--- src/local_net/src/address.rs
+++ src/local_net/src/address.rs
@@ -1,26 +1,12 @@
 use futures::TryStreamExt;
-use netlink_packet_route::address::Nla::Address;
 use netlink_packet_route::AddressMessage;
-use rtnetlink::Error::RequestFailed;
-use rtnetlink::Handle;
+use rtnetlink::{new_connection, Handle};
 use std::net::IpAddr;
 
-pub async fn get_addresses(
-    handle: &Handle,
-    iface_idx: u32,
-) -> Result<Vec<AddressMessage>, Box<dyn std::error::Error>> {
-    let mut addresses = handle
-        .address()
-        .get()
-        .set_link_index_filter(iface_idx)
-        .execute();
-    let mut addresses_vec = Vec::new();
-
-    while let Some(route_message) = addresses.try_next().await? {
-        addresses_vec.push(route_message);
-    }
-
-    Ok(addresses_vec)
+pub enum RTNetlinkError {
+    RTNetlink(rtnetlink::Error),
+    IOError(std::io::Error),
+    ValidationFailed,
 }
 
 pub async fn add_address(
@@ -28,44 +14,86 @@ pub async fn add_address(
     iface_idx: u32,
     address: IpAddr,
     prefix_len: u8,
-) -> Result<(), Box<dyn std::error::Error>> {
+) -> Result<(), RTNetlinkError> {
     let request = handle.address().add(iface_idx, address, prefix_len);
 
-    request.execute().await?;
+    request.execute().await.map_err(RTNetlinkError::RTNetlink)?;
 
     // Validate:
-    for res_addr in get_addresses(handle, iface_idx).await? {
-        match address {
-            IpAddr::V4(v4) => {
-                if res_addr.nlas.contains(&Address(v4.octets().to_vec())) {
-                    return Ok(());
-                }
-            }
-            IpAddr::V6(v6) => {
-                if res_addr.nlas.contains(&Address(v6.octets().to_vec())) {
-                    return Ok(());
-                }
-            }
+    let res_address = handle
+        .address()
+        .get()
+        .set_link_index_filter(iface_idx)
+        .set_address_filter(address)
+        .execute()
+        .try_next()
+        .await
+        .map_err(RTNetlinkError::RTNetlink)?;
+
+    match res_address {
+        Some(_) => Ok(()),
+        None => Err(RTNetlinkError::ValidationFailed),
+    }
+}
+
+pub async fn del_address(handle: &Handle, address: AddressMessage) -> Result<(), RTNetlinkError> {
+    let request = handle.address().del(address);
+    request.execute().await.map_err(RTNetlinkError::RTNetlink)?;
+
+    // Validation cannot be done at this moment because nlas feilds are private. We will have to
+    // manually modify this package to to validation.
+    Ok(())
+}
+
+pub async fn flush_addresses(handle: &Handle, iface_idx: u32) -> Result<(), RTNetlinkError> {
+    let mut request = handle
+        .address()
+        .get()
+        .set_link_index_filter(iface_idx)
+        .execute();
+
+    #[cfg(not(experimental = "threading"))]
+    {
+        let (connection, delete_handle, _) = new_connection().unwrap();
+        tokio::spawn(connection);
+        while let Some(address) = request
+            .try_next()
+            .await
+            .map_err(RTNetlinkError::RTNetlink)?
+        {
+            del_address(&delete_handle, address).await?
         }
     }
 
-    Err(Box::new(RequestFailed))
-}
+    #[cfg(experimental = "threading")]
+    {
+        use futures::future::join_all;
 
-pub async fn del_address(
-    handle: &Handle,
-    address: AddressMessage,
-) -> Result<(), Box<dyn std::error::Error>> {
-    Ok(handle.address().del(address).execute().await?)
-}
-
-pub async fn flush_addresses(
-    handle: &Handle,
-    iface_idx: u32,
-) -> Result<(), Box<dyn std::error::Error>> {
-    for addr in get_addresses(handle, iface_idx).await? {
-        handle.address().del(addr).execute().await?;
+        let futures = Vec::new();
+        while let Some(address) = request
+            .try_next()
+            .await
+            .map_err(RTNetlinkError::RTNetlink)?
+        {
+            let (connection, delete_handle, _) = new_connection().unwrap();
+            del_address(&delete_handle, address);
+            futures.push(tokio::spawn(connection));
+        }
+        join_all(futures).await?;
     }
 
-    Ok(())
+    // Validation
+    let res_address = handle
+        .address()
+        .get()
+        .set_link_index_filter(iface_idx)
+        .execute()
+        .try_next()
+        .await
+        .map_err(RTNetlinkError::RTNetlink)?;
+
+    match res_address {
+        Some(_) => Err(RTNetlinkError::ValidationFailed),
+        None => Ok(()),
+    }
 }
